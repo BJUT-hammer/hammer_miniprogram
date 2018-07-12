8894725f5c882595c9b31f11e5f0eca456a8c45b
SykieChen <chen17@coder17.com>
2018-07-13 04:01:29 +0800

    fixed mark query

--- pages/grade/grade.js
+++ pages/grade/grade.js
@@ -2,8 +2,8 @@
 Page({
 
   data: {
-    account1: "",
-    password1: "",
+    account1: "15071025",
+    password1: "sykiechen1994",
     src1: "https://chafen.bjut123.com/require_grade.php?account=",
     src2: "&password=",
     src3: "&current_year=2017-2018&current_term=2",
